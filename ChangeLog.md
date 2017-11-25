02a11fc7df89bc67a72c09e26edd4fb07af5c45c
Enzo Lebrun <enzo.the@gmail.com>
2017-11-25 17:33:39 +0100

    up

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,4 @@
-# Revision history for connect4
+# Revision history for puissance4
 
 ## 0.1.0.0  -- YYYY-mm-dd
 
--- src/Puissance4/Jarvis.hs
+++
@@ -1,5 +0,0 @@
-module Jarvis where
-  import Game
-
-  cost game =
-    countFromLast (pawns game) (head $pawns game)
